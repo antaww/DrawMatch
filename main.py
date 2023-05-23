2d4714a08bdd09d29b4783e362d76cf9b5d4d995
antaww <anta.pizzetta@gmail.com>
2023-05-23 16:17:03 +0200

    tweak(main): Include server starter in main.py.
    Closes #4

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from django.core.management import execute_from_command_line
+import os
 
 if __name__ == '__main__':
-    # Lancer le serveur Django
-    execute_from_command_line(['manage.py', 'runserver'])
+    os.system("python ./DrawMatch/manage.py runserver")
